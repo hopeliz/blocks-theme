c740e3a2a2e2affd559855e0331dc46392c73a68
Hope Moore <hopemoore@gmail.com>
2022-01-16 18:40:58 -0500

    Created artwork.html with css and js files

--- js/rollandstuff.js
+++ js/rollandstuff.js
@@ -44,4 +44,212 @@ $(document).ready(function() {
     $("#e-small").hover(function() {
         $("#text-header").html("EXPERIENCE");
     });
+    
+    // || Zooming in Artwork
+    
+    $("#the-ridges").hover(function() {
+        $("#the-ridges p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#the-ridges p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#the-ridges").hover(function() {
+        $("#the-ridges").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#the-ridges").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#rememblocksvr").hover(function() {
+        $("#rememblocksvr p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#rememblocksvr p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#rememblocksvr").hover(function() {
+        $("#rememblocksvr").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#rememblocksvr").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#tuning-out").hover(function() {
+        $("#tuning-out p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#tuning-out p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#tuning-out").hover(function() {
+        $("#tuning-out").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#tuning-out").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#sibyl").hover(function() {
+        $("#sibyl p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#sibyl p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#sibyl").hover(function() {
+        $("#sibyl").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#sibyl").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#dancing-colors").hover(function() {
+        $("#dancing-colors p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#dancing-colors p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#dancing-colors").hover(function() {
+        $("#dancing-colors").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#dancing-colors").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#music-sticky-notes").hover(function() {
+        $("#music-sticky-notes p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#music-sticky-notes p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#music-sticky-notes").hover(function() {
+        $("#music-sticky-notes").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#music-sticky-notes").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#turbulent-minds").hover(function() {
+        $("#turbulent-minds p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#turbulent-minds p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#turbulent-minds").hover(function() {
+        $("#turbulent-minds").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#turbulent-minds").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#scripps-poster").hover(function() {
+        $("#scripps-poster p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#scripps-poster p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#scripps-poster").hover(function() {
+        $("#scripps-poster").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#scripps-poster").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#shaders-textures").hover(function() {
+        $("#shaders-textures p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#shaders-textures p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#shaders-textures").hover(function() {
+        $("#shaders-textures").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#shaders-textures").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#glow-painting").hover(function() {
+        $("#glow-painting p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#glow-painting p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#glow-painting").hover(function() {
+        $("#glow-painting").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#glow-painting").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#balance-board").hover(function() {
+        $("#balance-board p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#balance-board p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#balance-board").hover(function() {
+        $("#balance-board").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#balance-board").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#animation").hover(function() {
+        $("#animation p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#animation p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#animation").hover(function() {
+        $("#animation").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#animation").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#arduino").hover(function() {
+        $("#arduino p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#arduino p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#arduino").hover(function() {
+        $("#arduino").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#arduino").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#touchdesigner").hover(function() {
+        $("#touchdesigner p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#touchdesigner p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#touchdesigner").hover(function() {
+        $("#touchdesigner").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#touchdesigner").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    
+    $("#unity").hover(function() {
+        $("#unity p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#unity p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#unity").hover(function() {
+        $("#unity").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#unity").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
+    
+    $("#mixed").hover(function() {
+        $("#mixed p").delay(100).animate({opacity: "1"}, 200, "swing");
+         }, function(){
+        $("#mixed p").animate({opacity: "0"}, 200, "swing");
+    });
+    
+    $("#mixed").hover(function() {
+        $("#mixed").animate({"background-size": "300px"}, 200, "swing");
+         }, function(){
+        $("#mixed").delay(100).animate({"background-size": "200px"}, 200, "swing");
+    });
 });
